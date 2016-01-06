b3a8e6e810f0b2395787301a51b98ca59ee1ebce
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2016-01-06 22:16:19 +0700

    1.property in class 2.interface and its property 3.one way binding 4.two way binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,29 @@
 import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My Third Angular 2 App</h1>'
+    template: `<h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div>
+    	<label>id: </label>{{hero.id}}
+    </div>
+    	<div>
+    		<label>name: </label>{{hero.name}}
+    		<div>
+    			<input [(ngModel)]="hero.name" placeholder="name">
+
+    		</div>
+    	</div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+	public title = 'Tour of Heros 2';
+	public hero = {
+		id: 1,
+		name: 'korn'
+	};
+}
+
+interface Hero {
+	id: number;
+	name: string;
+}
