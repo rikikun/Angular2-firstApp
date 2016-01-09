5d8fca8ac1c94b791e936813df057c802113e5b9
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2016-01-09 23:48:18 +0700

    finish heroes

--- app/heroes/heroes.component.ts
+++ app/heroes/heroes.component.ts
@@ -1,11 +1,31 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {Router} from 'angular2/router';
+import {Hero} from 'app/entity/hero';
+import {HeroService} from 'app/service/hero.service';
 @Component({
   selector: 'heroes',
   templateUrl: 'app/heroes/heroes.component.html',
+  styleUrls: ['app/heroes/heroes.component.css']
 })
-export class HerosComponent {
-	constructor() {}
-	public title = 'Tour of Heros 2';
+export class HerosComponent implements OnInit {
+	constructor(private _heroService:HeroService, private _router: Router) {}
+	public title = 'My Heroes';
+	public myHero: Hero;
+	public heroes: Hero[];
+
+	ngOnInit() {
+		this._heroService.getHeroes().then( heros => 
+			this.heroes=heros;
+		);
+	}
+
+	onSelect(hero) {
+		this.myHero = hero;
+	}
+
+	gotoDetail() {
+		this._router.navigate(['HeroDetail', { id: this.myHero.id }]);
+	}
 }
 
 
